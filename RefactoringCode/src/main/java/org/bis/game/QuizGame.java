fe3fed7d753615c78ff555de095589aedcc484d5
Javier <javier.delaconcepcion@gmail.com>
2025-05-24 09:28:07 +0200

    #Refactoring the rollDice method

--- RefactoringCode/src/main/java/org/bis/game/QuizGame.java
+++ RefactoringCode/src/main/java/org/bis/game/QuizGame.java
@@ -73,15 +73,15 @@ public class QuizGame {
         // Log to see the number of partipants
         
         for (Player _player : palyers) {
-            System.out.println("Participant: "+ _player.Name + " has joined the game at the position: " + _player.Position);
+            LogMessage("Participant: "+ _player.Name + " has joined the game at the position: " + _player.Position);
         }
         
-        System.out.println("Total players: "+ palyers.size());
+        LogMessage("Total players: "+ palyers.size());
         
-        // Checking if requisites are fulfilled before start the game
-        if(checkRequisites()){
-            
-            do {
+        do {
+             
+            // Checking if requisites are fulfilled before start the game
+            if(checkRequisites()){
                 // Simulate a rollDice
                 // Setting nextInt(5) + 1 to avoid 0.
                 gameInstance.rollDice(randomizer.nextInt(5) + 1); 
@@ -91,10 +91,14 @@ public class QuizGame {
                 } else {
                     gameRunning = gameInstance.answeredCorrectly();
                 }
+            }
+            else{
+                gameRunning = false;
+            }
 
-            } while (gameRunning);
+        } while (gameRunning);
 
-        }
+        
         
         
     }
@@ -110,45 +114,51 @@ public class QuizGame {
     */
     private void rollDice(int diceRoll) {
         
-        System.out.println(palyers.get(activePlayer).Name + " is currently playing");
-        System.out.println(palyers.get(activePlayer).Name + " rolled a " + diceRoll);
+        Player tmpPlayer = palyers.get(activePlayer);
+        
+        LogMessage(tmpPlayer.Name + " is currently playing");
+        LogMessage(tmpPlayer.Name + " rolled a " + diceRoll);
         
         if (palyers.get(activePlayer).HasPenalty) {
             if (diceRoll % 2 != 0) {
-                isExitingPenaltyBox = true;
                 
-                System.out.println(palyers.get(activePlayer) + " is leaving the penalty box");
-                positions[activePlayer] = positions[activePlayer] + diceRoll;
-                if (positions[activePlayer] > 11) positions[activePlayer] = positions[activePlayer] - 12;
+                palyers.get(activePlayer).HasPenalty = false;
+                //isExitingPenaltyBox = true;
                 
-                System.out.println(palyers.get(activePlayer) 
-                        + "'s new position is " 
-                        + positions[activePlayer]);
-                System.out.println("The current category is " + determineCategory());
+                LogMessage(tmpPlayer.Name + " is leaving the penalty box");
+               
+                // Increment the value for this player              
+                addingDiceValue(activePlayer,diceRoll);
+                
                 askQuestion();
+
             } else {
-                System.out.println(palyers.get(activePlayer) + " is staying in the penalty box");
-                isExitingPenaltyBox = false;
+                LogMessage(tmpPlayer.Name + " is staying in the penalty box");
+                //isExitingPenaltyBox = false;
             }
         } else {
-            positions[activePlayer] = positions[activePlayer] + diceRoll;
-            if (positions[activePlayer] > 11) positions[activePlayer] = positions[activePlayer] - 12;
+            // Increment the value for this player              
+            addingDiceValue(activePlayer,diceRoll);
             
-            System.out.println(palyers.get(activePlayer) + "'s new position is " 
-                    + positions[activePlayer]);
-            System.out.println("The current category is " + determineCategory());
             askQuestion();
         }
     }
     
     
     
-    
-    
-    
-    
     /*
-        Populate the list of questions
+      Method to add the dice Roll value into the active player
+    */
+    private void addingDiceValue(int _activePlayer, int _diceRoll) {
+        
+        palyers.get(activePlayer).Val = ( (palyers.get(activePlayer).Val + _diceRoll) > 11 ) ? ( palyers.get(activePlayer).Val + _diceRoll - 12 ) : palyers.get(activePlayer).Val + _diceRoll;
+        LogMessage(palyers.get(activePlayer).Name + "'s new position is " + palyers.get(activePlayer).Val);
+        LogMessage("The current category is " + determineCategory(palyers.get(activePlayer).Val));
+    }
+    
+
+    /*
+      Populate the list of questions 
     */
     private void creatingQuestions() {
         
@@ -163,6 +173,7 @@ public class QuizGame {
     
     /*
       List of required checks to keep running the game.
+      Return true if requisites to keep running are match, false in case execution needs to be finished
     */
     private static boolean checkRequisites(){
         
@@ -181,10 +192,31 @@ public class QuizGame {
         return true;
     }
     
+    /*
+      Log the input string on the Console
+    */
+    private static void LogMessage(String _meessage){
+        System.out.println(_meessage);
+    }
     
     
-    
-    
+    /*
+      Determine the questionCategory, based on the value
+      Return the category Name
+    */  
+    private String determineCategory( int _value) {
+        
+        if(_value % 4 == 0){
+            return "Literature";
+        }else if(_value % 4 == 1){
+            return "Music";
+        }else if(_value % 4 == 2) {
+            return "History";
+        }else{
+            return "Technology";
+        }
+            
+    }
     
     //endregion
     
@@ -206,18 +238,7 @@ public class QuizGame {
     }
     
     
-    private String determineCategory() {
-        if (positions[activePlayer] == 0) return "Literature";
-        if (positions[activePlayer] == 4) return "Literature";
-        if (positions[activePlayer] == 8) return "Literature";
-        if (positions[activePlayer] == 1) return "Music";
-        if (positions[activePlayer] == 5) return "Music";
-        if (positions[activePlayer] == 9) return "Music";
-        if (positions[activePlayer] == 2) return "History";
-        if (positions[activePlayer] == 6) return "History";
-        if (positions[activePlayer] == 10) return "History";
-        return "Technology";
-    }
+
 
     public boolean answeredCorrectly() {
         if (penaltyBoxStatus[activePlayer]){
